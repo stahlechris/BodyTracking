e27ab1dfb019da36b47bd81f2dfbe07a09b5cd27
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-08-10 21:16:05 -0500

    Second proj with k4a basics working

---
+++ GetTheCameraWorking/20180810_01_k4aAttempt/Assets/Scripts/LoadDllsOnStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+public class LoadDllsOnStart : MonoBehaviour
+{
+	[DllImport("DllHelloWorld")]
+	public static extern int DisplayHelloFromDLL();
+	[DllImport("k4a")]
+	public static extern uint k4a_device_get_installed_count();
+	
+	void Start()
+    {
+		Debug.Log("Time is " + DateTime.Now.ToString("h:mm:ss tt"));
+		Debug.Log(DisplayHelloFromDLL());
+		Debug.Log(k4a_device_get_installed_count());
+		
+		
+		Debug.Log("Exit");
+	}
+
+}
